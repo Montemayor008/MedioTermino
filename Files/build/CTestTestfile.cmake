92d20ec48d2b6e463b3fc7d0a3547bdc839e4fe4
Montemayor008 <montemayor008@gmail.com>
2023-05-26 10:06:11 -0600

    Update Files

---
+++ Files/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/meli/Documents/MedioTermino/Files/src
+# Build directory: /home/meli/Documents/MedioTermino/Files/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("image_package")
