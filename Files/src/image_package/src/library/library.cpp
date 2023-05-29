68e00c9b82a596aef8548691d18d002ffd7779c8
Melissa Montemayor Riojas <montemayor008@gmail.com>
2023-05-28 20:35:33 -0600

    5/28/2023

--- Files/src/image_package/src/library/library.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-// <!-- Pointers to the X Y Z coordinates -->
-extern "C" void calculateCoords(float* x, float* y, float* z) {
-    *x = (*x) * 100;
-    *y = (*y) * 100;
-    *z = (*z) * 100;
-    return;
-}
