080363c0942c58288796688ead072f2974b3b1e4
Melissa Montemayor Riojas <montemayor008@gmail.com>
2023-05-28 20:49:15 -0600

    Act Interfaces5/28

---
+++ Files/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH='/home/meli/Documents/MedioTermino/Files/devel:/opt/ros/noetic'
+export LD_LIBRARY_PATH='/opt/ros/noetic/lib'
+export PKG_CONFIG_PATH='/opt/ros/noetic/lib/pkgconfig'
+export PWD='/home/meli/Documents/MedioTermino/Files/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/meli/Documents/MedioTermino/Files/devel/share/common-lisp'
+export ROS_PACKAGE_PATH='/home/meli/Documents/MedioTermino/Files/src:/opt/ros/noetic/share'
